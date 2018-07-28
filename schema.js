439010ad93a7665f2e50b37411dc75b26abdd965
Michael Mallari <hello@michaelmallari.com>
2018-07-28 12:58:35 -0400

    Built initial RootQuery with hard-coded data

--- schema.js
+++ schema.js
@@ -1,12 +1,59 @@
 const {
-    GraphQLObjectType,
-    GraphQLString,
-    GraphQLInt,
-    GraphQLSchema,
-    GraphQLList,
-    GraphQLNonNull
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLNonNull
 } = require('graphql');
 
-module.exports = new GraphQLSchema({
 
+// Temporary hard-coded data
+const customers = [
+  {
+    id: '1',
+    name: 'John Doe',
+    email: 'john.doe@gmail.com',
+    age: 23
+  },
+  {
+    id: '2',
+    name: 'Jane Doe',
+    email: 'jane.doe@gmail.com',
+    age: 22
+  }
+]
+// Customer Type
+const CustomerType = new GraphQLObjectType({
+  name: 'Customer',
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  })
+});
+// Root query
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    customer: {
+      type: CustomerType,
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve(parentValue, args) {
+        for(let i = 0; i < customers.length; i++) {
+          if(customers[i].id == args.id) {
+            return customers[i];
+          }
+        }
+      }
+    }
+  }
+});
+
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const schema = require('./schema.js');
 const app = express();
 
 app.use('/graphql', expressGraphQL({
-    schema:schema,
-    graphiql:true
+  schema: schema,
+  graphiql: true
 }));
 
 app.listen(4000, () => {
-    console.log('Server is running on port 4000');
+   console.log('Server is running on port 4000');
 });
